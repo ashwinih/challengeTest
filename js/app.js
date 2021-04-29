288b53ad279c93477eb7dd9575e70f11f3ace670
ashwinih <ashwinih@techepoch.com>
2021-04-29 16:42:32 +0530

    function call on submit

--- js/app.js
+++ js/app.js
@@ -1,95 +1,5 @@
 $(document).ready(function() {
 
-    $("#btn-search-email").on("click", function(e) {
-        e.preventDefault();
-        localStorage.clear(); //Clears storage for next request
-        email = $('input[name="email"]').val().toLowerCase();
-
-        var x, y;
-        regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-        if (email.match(regEx)) {
-            x = true;
-            document.querySelector('input[name="email"]').parentNode.classList.remove("error");
-        } else {
-            x = false;
-        }
-
-        if (x === true) {
-            /* check network connction */
-            if (navigator.onLine == false) {
-                alert("Please check your internate connction");
-                return false;
-            }
-
-            showLoader(); //show loader
-			
-            document.querySelector('input[name="email"]').parentNode.classList.remove("error");
-            const proxyurl = "";
-            const url =
-                'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
-            fetch(proxyurl + url)
-                .then((response) => response.text())
-                .then(function(contents) {
-                    console.log("contents :: " + contents);
-                    localStorage.setItem("userObject", contents);
-                    window.location.href = "result.html";
-                })
-                .catch((e) => console.log(e));
-
-        } else if (x !== true) {
-            document.querySelector('input[name="email"]').parentNode.classList.add("error");
-        }
-    });
-
-    $("#btn-search-phone").on("click", function(e) {
-        e.preventDefault();
-        localStorage.clear(); //Clears storage for next request
-        phone = $('input[name="phone"]').val();
-        if (phone != null && phone != "") {
-            phone = phone.match(/\d/g, "");
-            if (phone == null) {
-                document.querySelector('input[name="phone"]').parentNode.classList.add("error");
-                return false;
-            }
-            phone = phone.join().replace(/,/g, "");
-        }
-
-        var x, y;
-        regEx = /^\d{10}$/;
-        if (phone.match(regEx)) {
-            x = true;
-            document.querySelector('input[name="email"]').parentNode.classList.remove("error");
-        } else {
-            x = false;
-        }
-
-        if (x === true) {
-            /* check network connction */
-            if (navigator.onLine == false) {
-                alert("Please check your internate connction");
-                return false;
-            }
-
-           showLoader(); //show loader
-		   
-            document.querySelector('input[name="phone"]').parentNode.classList.remove("error");
-            const proxyurl = "";
-            const url =
-                'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + phone;
-            fetch(proxyurl + url)
-                .then((response) => response.text())
-                .then(function(contents) {
-
-                    localStorage.setItem("userObject", contents);
-                    window.location.href = "result.html";
-                })
-                .catch((e) => console.log(e));
-
-        } else if (x !== true) {
-            document.querySelector('input[name="phone"]').parentNode.classList.add("error");
-        }
-    });
-
     $('input[name="email"]').keypress(function(event) {
         email = $('input[name="email"]').val().toLowerCase();
         regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -112,24 +22,10 @@ $(document).ready(function() {
 
 
             if (x === true) {
-                /* check network connction */
-                if (navigator.onLine == false) {
-                    alert("Please check your internate connction");
-                    return false;
-                }
-
+                networkCheck() // check network
                 showLoader(); //show loader
-				
-                const proxyurl = "";
-                const url =
-                    'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
-                fetch(proxyurl + url)
-                    .then((response) => response.text())
-                    .then(function(contents) {
-                        localStorage.setItem("userObject", contents);
-                        window.location.href = "result.html";
-                    })
-                    .catch((e) => console.log(e));
+                apiCall(); //call api
+
             } else if (x !== true) {
                 document.querySelector('input[name="email"]').parentNode.classList.add("error");
             }
@@ -137,7 +33,6 @@ $(document).ready(function() {
     });
 
     $('input[name="phone"]').keypress(function(event) {
-
         /* restrict special char */
         var regex = new RegExp("^[0-9]+$");
         var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
@@ -178,24 +73,10 @@ $(document).ready(function() {
 
 
             if (x === true) {
-                /* check network connction */
-                if (navigator.onLine == false) {
-                    alert("Please check your internate connction");
-                    return false;
-                }
-
+                networkCheck() // check network
                 showLoader(); //show loader
-				
-                const proxyurl = "";
-                const url =
-                    'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + phone;
-                fetch(proxyurl + url)
-                    .then((response) => response.text())
-                    .then(function(contents) {
-                        localStorage.setItem("userObject", contents);
-                        window.location.href = "result.html";
-                    })
-                    .catch((e) => console.log(e));
+                apiCall(); //call api
+
             } else if (x !== true) {
                 document.querySelector('input[name="phone"]').parentNode.classList.add("error");
             }
@@ -208,14 +89,98 @@ $(document).ready(function() {
         $("form .input-group").removeClass("error");
         $('input').val("");
     })
-	
-	function showLoader(){
-		$(".loader-container").show();
-		$(".above-the-fold").hide();
-		$(".features").hide();
-		$(".result").hide();
-		$(".search-again").hide();
-	}
 
 
 });
+
+/* show loader */
+function showLoader() {
+    $(".loader-container").show();
+    $(".above-the-fold").hide();
+    $(".features").hide();
+    $(".result").hide();
+    $(".search-again").hide();
+}
+
+/* submit form */
+function sumbitForm(type, e) {
+    var x, y, regEx;
+    e.preventDefault();
+    localStorage.clear(); //Clears storage for next request
+
+    if (type == 'email') {
+        var email = $('input[name="email"]').val().toLowerCase();
+        regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        if (email.match(regEx)) {
+            x = true;
+            document.querySelector('input[name="email"]').parentNode.classList.remove("error");
+        } else {
+            x = false;
+        }
+    } else {
+        phone = $('input[name="phone"]').val();
+        if (phone != null && phone != "") {
+            phone = phone.match(/\d/g, "");
+            if (phone == null) {
+                document.querySelector('input[name="phone"]').parentNode.classList.add("error");
+                return false;
+            }
+            phone = phone.join().replace(/,/g, "");
+        }
+        regEx = /^\d{10}$/;
+        if (phone.match(regEx)) {
+            x = true;
+            document.querySelector('input[name="email"]').parentNode.classList.remove("error");
+        } else {
+            x = false;
+        }
+    }
+
+    if (x === true) {
+        networkCheck() //check network connction
+        showLoader(); //show loader
+
+        if (type == "email") {
+            document.querySelector('input[name="email"]').parentNode.classList.remove("error");
+            apiCall(email); //call api
+        } else {
+            document.querySelector('input[name="phone"]').parentNode.classList.remove("error");
+            apiCall(phone); //call api
+        }
+
+    } else if (x !== true) {
+        if (type == "email") {
+            document.querySelector('input[name="email"]').parentNode.classList.add("error");
+        } else {
+            document.querySelector('input[name="phone"]').parentNode.classList.add("error");
+        }
+    }
+}
+
+/* check network connection */
+function networkCheck() {
+    if (navigator.onLine == false) {
+        alert("Please check your internet connection");
+        return false;
+    }
+}
+
+/* call api */
+function apiCall(type) {
+    const proxyurl = "";
+    var url = "";
+    if (type == "email") {
+        url = 'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
+    } else {
+        url = 'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + phone;
+    }
+
+    fetch(proxyurl + url)
+        .then((response) => response.text())
+        .then(function(contents) {
+            console.log("contents :: " + contents);
+            localStorage.setItem("userObject", contents);
+            window.location.href = "result.html";
+        })
+        .catch((e) => console.log(e));
+}
